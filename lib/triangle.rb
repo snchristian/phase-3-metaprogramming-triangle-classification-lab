e5ffff27158d6c0c88c198019b1721f1d42eb68d
Shantel <snchristian8@gmail.com>
2022-02-28 15:46:14 -0500

    commit tree 3f405bff68660b1b7a9bc8e5c29e7e5e5b03d759

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
 class Triangle
-  # write code here
+  
+  def initialize(a,b,c)
+    @a=a
+    @b=b
+    @c=c
+    if @a == 0 or @c == -5 or @a + @b <= @c or @a + @c <= @b or @b + @c <= @a
+       raise TriangleError
+      end
+  end
+
+class TriangleError < StandardError
+  end
+
+  def kind()
+    if @a == @b && @a == @c
+      :equilateral
+    elsif@a == @b|| @a == @c || @c == @b
+      :isosceles
+    else
+      :scalene
+  end
+  end
 end
+
+tri4= Triangle.new(60,60,60)
